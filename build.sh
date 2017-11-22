09c79a610a5f69c937fef088a84a88565aff3fd5
Jordan Earls <earlz@earlz.net>
2017-11-22 16:37:21 -0500

    Make generic freestanding build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export PREFIX="$HOME/opt/cross"
+export TARGET=i386-qtum
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir build-binutils
+cd build-binutils
+../binutils-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+cd ..
+mkdir build-gcc
+cd build-gcc
+../gcc-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+
